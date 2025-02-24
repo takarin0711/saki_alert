8ecfc2589d58d7d3a06b5a5d909a6e361e51a79c
Takayuki Inohiza <takayuki_0711sdx@yahoo.co.jp>
2025-02-24 18:23:21 +0900

    fix

--- saki_alert.py
+++ saki_alert.py
@@ -1,7 +1,12 @@
 import datetime
+import locale
 
 def time_based_comment():
+    locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
     now = datetime.datetime.now()
+    formatted_date = now.strftime('%-m月%-d日(%a) %-H時%-M分')
+    print(formatted_date)
+    print("(咲季): ", end="")
     if 4 <= now.hour < 5:
         print("朝の4時よ！これから走りに出るんでしょ？")
     elif 20 <= now.hour < 21:
@@ -9,7 +14,8 @@ def time_based_comment():
     elif now.weekday() == 6 and 12 <= now.hour < 13:
         print("昼寝をするわ！")
     else:
-        print("testです！")
+        print("お姉ちゃんに任せなさいっ!")
+    print()
 
 def main():
     time_based_comment()
